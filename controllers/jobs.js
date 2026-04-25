d7324546011797cf2a27828b6c97d42d4d281fef
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-25 23:23:51 +0300

    feat(jobs): implement monthly job application stats
    - Calculate monthly job application stats
    - Aggregate jobs by year and month
    - Format dates using moment.js

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,6 +2,7 @@ const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 const getAllJobs = async (req, res) => {
   const { search, status, jobType, sort } = req.query;
@@ -134,7 +135,38 @@ const showStats = async (req, res) => {
     interview: stats.interview || 0,
     declined: stats.declined || 0,
   };
-  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications: [] });
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $sort: { "_id.year": -1, "_id.month": -1 },
+    },
+    {
+      $limit: 6,
+    },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+
+      return { count, date };
+    })
+    .reverse();
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 module.exports = {
