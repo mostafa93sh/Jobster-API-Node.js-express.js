96c5ad2978f731db432325b5d84da7dbd9e9fdb1
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-25 18:37:44 +0300

    feat(jobs): add job sorting options
    - Sort by latest or oldest creation date
    - Sort by position A-Z or Z-A

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -22,6 +22,21 @@ const getAllJobs = async (req, res) => {
 
   let result = Job.find(queryObject);
 
+  // chain sort conditions
+
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    result = result.sort("position");
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+
   const jobs = await result;
 
   // const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
